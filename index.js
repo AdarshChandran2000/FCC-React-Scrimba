ba68797e317ab32c4800fda64d00d02c310ef0e8
AdarshChandran2000 <lachikku2210@gmail.com>
2022-01-15 23:20:34 +0300

    React the easy way

---
+++ index.js
@@ -0,0 +1,15 @@
+function Content() {
+    return (<h3>I m doing great.</h3>);
+}
+
+
+
+ReactDOM.render(
+    <div>
+        <h1>
+            Hello, Everyone
+        </h1>
+        <Content /> 
+    </div>,
+    document.getElementById("root")
+);
