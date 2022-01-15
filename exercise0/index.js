3dad5ce3e3fa6a48c387d3920b857c98b87fd0eb
AdarshChandran2000 <lachikku2210@gmail.com>
2022-01-16 01:11:41 +0300

    Part1

---
+++ exercise0/index.js
@@ -0,0 +1,16 @@
+const element = (
+    <nav>
+        <h1> Adarsh </h1>
+        <ul>
+            <li> Pricing </li>
+            <li> About </li>
+            <li> Contact </li>
+        </ul>
+    </nav>
+);
+
+
+ReactDOM.render(
+    element,
+    document.getElementById("root")
+);
---
+++ exercise1/index.js
@@ -0,0 +1,19 @@
+const app = (
+    <div>
+        <img alt="react.png"/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+
+        </ul>
+    </div>
+);
+
+ReactDOM.render(
+    app,
+    document.getElementById('root')
+);
